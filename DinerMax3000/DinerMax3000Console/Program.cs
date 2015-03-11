b8371c8a1c71a76fcebe96eb0ef031c7a9323e23
Andrew Eloff <andeloff@gmail.com>
2015-03-11 18:17:53 -0500

    Hello World
    Hello World!

--- DinerMax3000/DinerMax3000Console/Program.cs
+++ DinerMax3000/DinerMax3000Console/Program.cs
@@ -10,6 +10,8 @@ namespace DinerMax3000Console
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Hello World");
+            Console.ReadKey();
         }
     }
 }
