470bb50128ee4f7b8920bc7610d013abb54dc594
Andrew Eloff <andeloff@gmail.com>
2015-03-12 22:49:05 -0500

    Copying Project to Branch
    Currently bug free

--- DinerMax3000/DinerMax3000Console/Program.cs
+++ DinerMax3000/DinerMax3000Console/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using DinerMax3000BusinessLayer;
 
 namespace DinerMax3000Console
 {
@@ -10,8 +11,41 @@ namespace DinerMax3000Console
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World");
+            FoodMenu summerMenu = new FoodMenu();
+            summerMenu.Name = "Summer Menu";
+            summerMenu.AddMenuItem("Item A", "Item A Description", 25.50);
+            summerMenu.AddMenuItem("Item B", "Item B Description", 25.50);
+            summerMenu.HosptialDirections = "Right around the corner";
+
+            DrinksMenu drinkMenu = new DrinksMenu();
+            drinkMenu.Disclaimer = "Don't drink and code";
+            drinkMenu.AddMenuItem("Whiskey Coke", "It'll get you hammered", 5);
+            drinkMenu.AddMenuItem("Rum and Coke", "It'll get you drunk", 5);
+
+            Order hungryGuestOrder = new Order();
+            for (int i = 0; i < summerMenu.items.Count - 1; i++)
+            {
+                MenuItem currentItem = summerMenu.items[i];
+                hungryGuestOrder.items.Add(currentItem);
+            }
+
+            foreach (MenuItem currentItem in drinkMenu.items)
+            {
+                hungryGuestOrder.items.Add(currentItem);
+            } 
+
+
+            Console.WriteLine("The total is: " + hungryGuestOrder.Total);
+            try
+            {
+                drinkMenu.AddMenuItem("Words Here", "Even more words here", 0);
+            }
+            catch (Exception thrownException)
+            {
+                Console.WriteLine(thrownException.Message);
+            }
             Console.ReadKey();
+            
         }
     }
 }
