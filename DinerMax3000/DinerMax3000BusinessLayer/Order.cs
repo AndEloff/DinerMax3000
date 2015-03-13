e379f9af22c6ebfd350117d5b124b8e5ba3656ef
Andrew Eloff <andeloff@gmail.com>
2015-03-12 22:38:35 -0500

    Setting Up Business Layer

---
+++ DinerMax3000/DinerMax3000BusinessLayer/Order.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DinerMax3000BusinessLayer
+{
+    public class Order
+    {
+        public List<MenuItem> items = new List<MenuItem>();
+
+        public double Total
+        {
+            get 
+            {
+                double calcTotal = 0;
+                foreach (MenuItem item in items)
+                {
+                    calcTotal = calcTotal + item.Price;
+                }
+                return calcTotal;
+            }
+        }
+    }
+}
