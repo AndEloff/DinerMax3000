1bbb95a8e4676ddc720b5d8ab49a837edc91d3d1
Andrew Eloff <andeloff@gmail.com>
2015-03-22 18:36:28 -0500

    Adding ASP Web Page
    Needs CSS love but it builds and I can use this to mess around

---
+++ DinerMax3000/DinerMax3000WebPage/Default.aspx
@@ -0,0 +1,11 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/DinerMax3000MasterPage.master" AutoEventWireup="true" CodeFile="Default.aspx.cs" Inherits="_Default" %>
+
+<asp:Content ID="Content1" ContentPlaceHolderID="head" Runat="Server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="cphMainContent" Runat="Server">
+
+    Welcome to Diner Max 3000
+    <br/>
+    We have some lunch specials and such. Check out our awesome food and drinks!
+</asp:Content>
+
---
+++ DinerMax3000/DinerMax3000WebPage/Menus.aspx.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class Menus : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        if (Session["MenusPageCount"] == null)
+        {
+            Session["MenusPageCount"] = 0;
+        }
+        Session["MenusPageCount"] = (int) Session["MenusPageCount"]+1;
+    }
+    protected void gvMenu_RowDataBound(object sender, GridViewRowEventArgs e)
+    {
+        if (e.Row.RowType == DataControlRowType.DataRow)
+        {
+            //This is how the inner Grid is going to pull in its data from the Bussiness Layer for Menu Items
+            GridView innerGridView = (GridView)e.Row.FindControl("gvMenuItems");
+            //THis is setting the data for that inner grid to the actual menu items
+            innerGridView.DataSource = ((DinerMax3000.BusinessLayer.Menu)e.Row.DataItem).items;
+            innerGridView.DataBind();
+        }
+    }
+}
